195276f94b66188e3171e0498040e4859a4a2f91
MohdAbdulGhani-dev <mohdabdulghani177@gmail.com>
2025-02-10 11:45:06 +0530

    the first program

--- src/program/Program.java
+++ src/program/Program.java
@@ -4,7 +4,7 @@ package program;
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("hello ll");
+        System.out.println("hello github");
     }
     
 }
