9f0f1090e8716d6e028bb84bcb605f2c513c39e3
MohdAbdulGhani-dev <mohdabdulghani177@gmail.com>
2025-02-10 11:47:43 +0530

    the addition program

---
+++ src/addtion/addition.java
@@ -0,0 +1,10 @@
+package addtion;
+
+public class addition {
+    
+    public static void main(String[] args) {
+        int a=2,b=3;
+        System.out.println("addition of a and b is "+(a+b));
+        
+    }
+}
