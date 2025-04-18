a8d2162eb9c83bed1c3288a4051b499020cc7934
MA-FerozAli <mohammadferozali7866@gmail.com>
2025-04-18 20:52:52 +0530

    Update addition.java

--- src/addtion/addition.java
+++ src/addtion/addition.java
@@ -5,6 +5,6 @@ public class addition {
     public static void main(String[] args) {
         int a=2,b=3;
         System.out.println("addition of a and b is "+(a+b));
-        
+        fffhshtrjuhhytvyth
     }
 }
